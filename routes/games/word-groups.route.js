4edc852f13475365de2e85d6779325d5dc20a3ad
D.F. Glover <dfg@boxedfx.com>
2026-05-10 07:49:11 +0700

    applied color styling

--- routes/games/word-groups.route.js
+++ routes/games/word-groups.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/games/word-groups', (req, res) => {
-  res.render('games/word-groups', {title: 'Word Groups',restart: 'wg-restart-btn', newGame: 'wg-new-game-btn', answers: 'wg-answers-btn', dropdown: 'wg-dropdown', shuffle: 'wg-shuffle-btn', clear: 'wg-clear-btn', submit: 'wg-submit-btn', currentPage: 'Word Groups'});
+  res.render('games/word-groups', {title: 'Word Groups',restart: 'wg-restart-btn', newGame: 'wg-new-game-btn', answers: 'wg-answers-btn', dropdown: 'wg-dropdown', shuffle: 'wg-shuffle-btn', clear: 'wg-clear-btn', submit: 'wg-submit-btn', currentPage: 'Word Groups',gameMessage: 'wg-Messsage Placeholder'});
 });
 
 export default router;
